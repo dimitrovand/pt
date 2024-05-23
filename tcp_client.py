a31a5b6c1210304a9625635b7916e4c4eaab060d
andrian dimitrov <andrian.dimitrov94@gmail.com>
2024-05-23 11:41:32 +0300

    impr

--- tcp_client.py
+++ tcp_client.py
@@ -1,17 +1,30 @@
 from socket import socket, AF_INET, SOCK_STREAM
-
-HOST = 'localhost'
-PORT = 50001
+from argparse import ArgumentParser
 
 
-if __name__ == "__main__":
+def main(host, port):
     while True:
-        with socket(AF_INET, SOCK_STREAM) as s:
-            s.connect((HOST, PORT))  
+        try:
+            with socket(AF_INET, SOCK_STREAM) as s, s.makefile('r') as rf, s.makefile('w') as wf:
+                s.settimeout(2)
+                s.connect((host, port))
 
-            with s.makefile('r') as rfile:
                 while True:
                     m = input('tx msg: ')
-                    s.sendall((m + '\n').encode()) 
-                    m = rfile.readline().strip()
+                    wf.write(m + '\n')  # Ensure the message is newline terminated
+                    wf.flush()  # Flush the buffer to send the data immediately
+                    m = rf.readline().strip()
                     print("rx msg:", m)
+        except (KeyboardInterrupt, TypeError):
+            break
+        except Exception as e:
+            print(f'{e}')
+
+
+if __name__ == '__main__':
+    ap = ArgumentParser(description='Simple tcp client')
+    ap.add_argument('host', type=str, help='The hostname or IP address of the server to connect to')
+    ap.add_argument('port', type=int, help='The port number on which the server is listening')
+    args = ap.parse_args()
+    main(args.host, args.port)
+
--- tcp_server.py
+++ tcp_server.py
@@ -1,9 +1,8 @@
 from socketserver import ThreadingTCPServer, StreamRequestHandler
-from threading import Thread
+from threading import Thread, Event
 from signal import signal, SIGINT
-
-HOST = 'localhost'
-PORT = 50001
+from select import select
+from argparse import ArgumentParser
 
 
 class RequestHandler(StreamRequestHandler):
@@ -13,32 +12,59 @@ class RequestHandler(StreamRequestHandler):
 
     def handle(self):
         while True:
-            try:
-                command = self.rfile.readline()
-            except TimeoutError:
+            # Use select() with small timeout to increase 
+            # the responsiveness to shutdown requests.
+            rdy_to_read, _, _ = select([self.rfile], [], [], 1)
+            
+            if self.server.shutdown_flag:
                 break
 
-            if not command:
-                break
-
-            self.wfile.write(command)
+            if self.rfile in rdy_to_read:
+                try:
+                    data = self.rfile.readline()
+                    if not data:
+                        break
+                    self.wfile.write(data)
+                except (TimeoutError, ConnectionAbortedError):
+                    break
 
 
 class Server(ThreadingTCPServer):
-    pass
+    def __init__(self, server_address, RequestHandlerClass):
+        super().__init__(server_address, RequestHandlerClass)
+        self.shutdown_flag = False
+
+    def shutdown(self):
+        self.shutdown_flag = True
+        super().shutdown()
+        
+
+def termination_handler(server, term_event):
+    term_event.wait()
+    server.shutdown()
 
 
-if __name__ == "__main__":
-    server = Server((HOST, PORT), RequestHandler)
+def main(host, port):
+    server = Server((host, port), RequestHandler)
 
-    def termination_handler():
-        server.shutdown()
+    # As specified in the socketserver docs, we need a 
+    # separate thread to shutdown the server.
+    term_event = Event()
+    term_listener = Thread(target=termination_handler, args=(server, term_event))
+    term_listener.start()
 
-    termination = Thread(target=termination_handler)
-
-    def sigint_handler(signal, frame):
-        termination.start()
+    def sigint_handler(*_):
+        term_event.set()
 
     signal(SIGINT, sigint_handler)
-    server.serve_forever()
-    termination.join()
+
+    server.serve_forever() # Handle one request at a time until shutdown.
+    term_listener.join()
+
+
+if __name__ == '__main__':
+    ap = ArgumentParser(description='Simple tcp server')
+    ap.add_argument('host', type=str, help='The hostname or IP address to bind the server to')
+    ap.add_argument('port', type=int, help='The port number on which the server will listen')
+    args = ap.parse_args()
+    main(args.host, args.port)
